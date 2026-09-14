3c5074aa1c1ab1b4e7a677be82d5acf233816038
repeter <vad1m200@mail.ru>
2026-09-14 15:58:22 +0300

    update interceptor common jwt pkg

--- pkg/auth/interceptor.go
+++ pkg/auth/interceptor.go
@@ -5,14 +5,37 @@ import (
 	"errors"
 	"strings"
 
+	authv1 "github.com/repeter513/shop-proto/gen/go/auth/v1"
+	catalogv1 "github.com/repeter513/shop-proto/gen/go/catalog/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 )
 
-func UnaryServerInterceptor(secret []byte) grpc.UnaryServerInterceptor {
-	return func(ctx context.Context, req any, _ *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
+var PublicAuth = []string{
+	authv1.AuthService_RegisterUser_FullMethodName,
+	authv1.AuthService_LoginUser_FullMethodName,
+	authv1.AuthService_ValidateToken_FullMethodName,
+	authv1.AuthService_RefreshToken_FullMethodName,
+}
+
+var PublicCatalog = []string{
+	catalogv1.CatalogService_GetProduct_FullMethodName,
+	catalogv1.CatalogService_ListProducts_FullMethodName,
+	catalogv1.CatalogService_ListCategories_FullMethodName,
+	catalogv1.CatalogService_GetStock_FullMethodName,
+}
+
+func UnaryServerInterceptor(secret []byte, public ...string) grpc.UnaryServerInterceptor {
+	skip := make(map[string]struct{}, len(public))
+	for _, m := range public {
+		skip[m] = struct{}{}
+	}
+	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
+		if _, ok := skip[info.FullMethod]; ok {
+			return handler(ctx, req)
+		}
 		userID, err := userIDFromMetadata(ctx, secret)
 		if err != nil {
 			return nil, status.Error(codes.Unauthenticated, err.Error())
